aca3018d6479edcdf6351cff2e43487cd0c6ff54
ccedit <ccedit@126.com>
2023-09-05 02:07:18 +0800

    first commit

--- components/DvImage/index.tsx
+++ components/DvImage/index.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
 interface ButtonProp {
   [key: string]: any
 }
 export default function DvImage({ attribute = {}, api = {}, event = [] }: ButtonProp) {
-  const { className = '', options = {}, position = {}} = attribute;
-  return <img className='w-full h-full border-0' {...{...options, src: options.src || '/illustrations/upload.png'}}/>;
+  const { options = {}} = attribute;
+  return <Image
+          src={options.src || '/illustrations/upload.png'}
+          alt={options.alt || ''}
+          fill
+          sizes='100vw'
+          style={{
+            objectFit: 'contain'
+          }}
+        />;
 }
